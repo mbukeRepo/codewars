b38575680bf63b6020e07538107fb7b7f5d4cc83
mbukeRepo <mbuke404@gmail.com>
2022-09-09 09:02:02 +0200

    added implementation for

---
+++ 5 kyu/movingZerosToTheEnd.js
@@ -0,0 +1,13 @@
+function moveZeros(arr) {
+  const zeros = [];
+  for(let i =0; i < arr.length; i++){
+    if (arr[i] === 0){
+      zeros.push(...arr.splice(i,1))
+      i--;
+    }
+  }
+  return arr.concat(zeros)
+}
+
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]));
