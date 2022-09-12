6dda1065c817074a24d19f900ecf457354ab8c43
mbukeRepo <mbuke404@gmail.com>
2022-09-12 13:17:27 +0200

    added basic tests

---
+++ 5 kyu/weightForWeight.test.js
@@ -0,0 +1,9 @@
+const orderWeight = require('./weightForWeight');
+describe('Order Weights', function () {
+  it('Basic tests', function () {
+    expect(orderWeight('103 123 4444 99 2000')).toEqual('2000 103 123 4444 99');
+    expect(
+      orderWeight('2000 10003 1234000 44444444 9999 11 11 22 123')
+    ).toEqual('11 11 2000 10003 22 123 1234000 44444444 9999');
+  });
+});
